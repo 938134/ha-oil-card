281ee6651e52f1b24d0e488f235a50dbc05ad74b
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-02-05 16:57:01 +0800

    Create custom_card.js

---
+++ dist/custom_card.js
@@ -0,0 +1,47 @@
+// custom_card.js
+class HaOilCard extends HTMLElement {
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error("Invalid config: 'entity' is required.");
+    }
+    this.config = config;
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <ha-card>
+        <div class="oil-grid">
+          <div class="header-row">
+            <img class="sinopec-logo" src="https://www.jkrunning.site/images/sinopec-logo.png" alt="中国石化">
+            <div class="price-tips">{{ states('sensor.oilprice_zhejiang_tips') }}</div>
+          </div>
+          <div class="price-row">
+            ${this.config.oil_prices.map(price => `
+              <div class="price-item">
+                <span class="oil-icon">⛽</span>
+                <div class="oil-type">${price.type}</div>
+                <div class="oil-price">{{ states('${price.entity}') }}</div>
+              </div>
+            `).join('')}
+          </div>
+          <div class="triple-row">
+            <div class="gas-item">
+              <div class="gas-icon-wrapper">
+                <span class="gas-icon">🔥</span>
+              </div>
+              <div class="gas-content">
+                <div class="gas-label">${this.config.gas_balance.label}</div>
+                <div class="gas-value">
+                  {{ states('${this.config.gas_balance.entity}') }}
+                </div>
+              </div>
+            </div>
+            <div class="empty-column"></div>
+          </div>
+        </div>
+      </ha-card>
+    `;
+  }
+}
+
+customElements.define("ha-oil-card", HaOilCard);

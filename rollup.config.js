c9ae95608589a5da7c37fee07b7f9173c46b1252
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-02-06 11:33:59 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'src/ha-oil-card.js',
-  output: {
-    file: 'dist/ha-oil-card.js',
-    format: 'es',
-  },
-  plugins: [
-    nodeResolve(),
-  ],
+    input: 'src/ha-oil-card.js',
+    output: {
+        file: 'dist/ha-oil-card.js',
+        format: 'es',
+    },
+    plugins: [
+        nodeResolve(),
+    ],
 };

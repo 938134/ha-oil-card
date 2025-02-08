5d82d49002ccabf90664db7934f1530638c7ffff
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-02-08 23:56:19 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/oil-card.js',
